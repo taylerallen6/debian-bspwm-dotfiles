3f059c747168793ce6cb77faafe7ddfe98a1655a
taylerallen6 <taylerallen6@gmail.com>
2024-01-01 21:33:59 -0600

    remove .local/share/nvim

--- misc_installs.sh
+++ misc_installs.sh
@@ -22,7 +22,6 @@ firefox \
 ### neovim
 curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
 chmod u+x nvim.appimage
-./nvim.appimage
 sudo mv ./nvim.appimage /usr/local/bin/nvim
 
 ### rustc cargo
